5005f17ce1c37549d868697dabe310ebfab708fc
Richard Todd <rwtodd@users.noreply.github.com>
2022-11-20 10:24:23 -0600

    Added YMDArg argument type for time-less dates
    this type is useful in multiple programs, so I'm adding it to the library.  It does
    force a dependency on Foundation, but realistically most programs will have that
    dependency anyway.

--- Sources/ArgParser/StringConvertibles.swift
+++ Sources/ArgParser/StringConvertibles.swift
@@ -6,6 +6,8 @@
 //
 //  Created by Richard Todd on 11/19/22.
 //
+import Foundation
+
 
 /**
   An argument type that accepts lists and ranges of integers.
@@ -34,12 +36,86 @@ public struct IntegerSequenceArg : LosslessStringConvertible {
         seq = ranges.compactMap({$0}).joined()
     }
     
-    // provide a default constructor that's empty
-    public init() {
-        seq = [].joined()
+    // provide a default constructor where ranges can be supplied
+    public init(_ ranges: [ClosedRange<Int>] = []) {
+        seq = ranges.joined()
     }
     
     public var description: String {
         seq.map { $0.description }.joined(separator: ",")
+    }
+}
+
+
+/**
+ A  wrapper for  for timeless Year-Month-Day dates that is
+ `LosslessStringConvertible`  and `Comparable`.
+ 
+  The input format is yyyy-mm-dd, where the year and month can be optionally
+ elided if they match the year and month from today.  The user can also input constants
+ 'today', 'tomorrow', and 'yesterday' if desired.
+ */
+public struct YMDArg : LosslessStringConvertible, Comparable {
+    public static func < (lhs: YMDArg, rhs: YMDArg) -> Bool {
+        lhs.date < rhs.date
+    }
+    
+    public let date : Date
+    
+    public init?(_ description: String) {
+        let calendar = Calendar.current
+        let now = Date()
+        let parts = description.split(separator: "-").map { Int($0) }
+        guard !parts.contains(nil) else {
+            switch description.lowercased() {
+            case "yesterday":
+                date = calendar.startOfDay(for: calendar.date(byAdding: .day, value: -1, to: now)!)
+            case "today":
+                date = calendar.startOfDay(for: now)
+            case "tomorrow":
+                date = calendar.startOfDay(for: calendar.date(byAdding: .day, value: 1, to: now)!)
+            default:
+                return nil
+            }
+            return
+        }
+        
+        var dc = calendar.dateComponents([.year,.month,.day], from: now)
+        switch parts.count {
+        case 3:
+            dc.year = parts[0]
+            dc.month = parts[1]
+            dc.day = parts[2]
+        case 2:
+            dc.month = parts[0]
+            dc.day = parts[1]
+        case 1:
+            dc.day = parts[0]
+        default:
+            return nil
+        }
+        
+        // try to rule out ridiculous dates since DateComponents is extremely forgiving
+        if dc.day! > 31 || dc.month! > 12 || dc.day! < 1 || dc.month! < 1 {
+            return nil
+        }
+        guard let date = calendar.date(from: dc) else { return nil }
+        self.date = date
+    }
+
+    public init(_ d: Date = Date()) {
+        date = Calendar.current.startOfDay(for: d)
+    }
+    
+    public init?(year: Int, month: Int, day: Int) {
+        let dc = DateComponents(year:year,month:month,day:day)
+        guard let specified = Calendar.current.date(from:dc) else { return nil }
+        date = specified
+    }
+    
+    public var description: String {
+        let cal = Calendar(identifier: .gregorian)
+        let dc = cal.dateComponents([.year,.month,.day], from: date)
+        return "\(dc.year!)-\(dc.month!)-\(dc.day!)"
     }
 }
--- Tests/ArgParserTests/StringConvertibleTests.swift
+++ Tests/ArgParserTests/StringConvertibleTests.swift
@@ -1,3 +1,4 @@
+import Foundation
 import XCTest
 @testable import ArgParser
 
@@ -75,5 +76,87 @@ final class IntSeqArgTests : XCTestCase {
         try isa!.process(param: "isa", arg: "")
         XCTAssertEqual(isa!.value.description, "")
     }
+    
+    func testArgConstructor() throws {
+        let arg = IntegerSequenceArg([1...3,6...6])
+        XCTAssertEqual(arg.description, "1,2,3,6")
+    }
 
+}
+
+
+/**
+ Tessts of DateArg
+ */
+final class YMDArgTests : XCTestCase {
+    var da : BasicParam<YMDArg>?
+    
+    override func setUp() {
+        super.setUp()
+        da = BasicParam<YMDArg>(names: ["da"], initial: YMDArg(), help: "")
+    }
+    
+    func testDefaultSetting() throws {
+        let today = Calendar.current.startOfDay(for: Date())
+        XCTAssertEqual(da!.value.date, today)
+    }
+    
+    func testSimpleDates() throws {
+        try da!.process(param: "da", arg: "2011-01-01")
+        XCTAssertEqual(da!.value, YMDArg(year: 2011, month: 1, day: 1)!)
+        try da!.process(param: "da", arg: "1977-06-02")
+        XCTAssertEqual(da!.value, YMDArg(year: 1977, month: 6, day: 2)!)
+    }
+    
+    func testAbbreviatedDates() throws {
+        let dc = Calendar.current.dateComponents([.year,.month], from: Date())
+        try da!.process(param: "da", arg: "11-14")
+        XCTAssertEqual(da!.value.description, "\(dc.year!)-11-14")
+        
+        try da!.process(param: "da", arg: "14")
+        XCTAssertEqual(da!.value.description, "\(dc.year!)-\(dc.month!)-14")
+    }
+    
+    func testSpecialArgs() throws {
+        let today = Calendar.current.startOfDay(for: Date())
+        try da!.process(param: "da", arg: "Today")
+        XCTAssertEqual(da!.value.date, today)
+        
+        let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: today)!
+        try da!.process(param: "da", arg: "yesterDAY")
+        XCTAssertEqual(da!.value, YMDArg(yesterday))
+        
+        let tomorrow =  Calendar.current.date(byAdding: .day, value: 1, to: today)!
+        try da!.process(param: "da", arg: "TOmorroW")
+        XCTAssertEqual(da!.value, YMDArg(tomorrow))
+    }
+    
+    func testBadDates() throws {
+        XCTAssertNil(YMDArg("2011-21-blah"))
+        XCTAssertNil(YMDArg(""))
+        XCTAssertNil(YMDArg("1-2-2011"))
+        XCTAssertNil(YMDArg("12-32"))
+        XCTAssertNil(YMDArg("0-0-0"))
+        XCTAssertNil(YMDArg("2018-0-14"))
+    }
+    
+    func testClampableDates() throws {
+        let cda = ClampedRangeParam(
+            names: ["date"],
+            initial: YMDArg(year:2022,month:12,day:1)!,
+            min: YMDArg(year:2022,month:11,day:15)!,
+            max: YMDArg(year:2022,month:12,day:15)!,
+            help: "between nov 15 and dec 15 in year 2022")
+        XCTAssertEqual(cda.value, YMDArg(year:2022,month:12,day:1))
+        try cda.process(param: "date", arg: "2022-11-14")
+        XCTAssertEqual(cda.value, YMDArg(year:2022,month:11,day:15))
+        try cda.process(param: "date", arg: "2012-1-1")
+        XCTAssertEqual(cda.value, YMDArg(year:2022,month:11,day:15))
+        try cda.process(param: "date", arg: "2000-12-3")
+        XCTAssertEqual(cda.value, YMDArg(year:2022,month:11,day:15))
+        try cda.process(param: "date", arg: "2023-12-1")
+        XCTAssertEqual(cda.value, YMDArg(year:2022,month:12,day:15))
+        try cda.process(param: "date", arg: "2022-12-16")
+        XCTAssertEqual(cda.value, YMDArg(year:2022,month:12,day:15))
+    }
 }
