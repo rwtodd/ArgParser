d6c623dd7a720f2c1a03d715bfc07effb803d931
Richard Todd <rwtodd@users.noreply.github.com>
2024-11-01 23:07:37 -0500

    converted test suite to Testing framework

--- Tests/ArgParserTests/ArgParserTests.swift
+++ Tests/ArgParserTests/ArgParserTests.swift
@@ -1,140 +1,156 @@
-import XCTest
-@testable import ArgParser
+import Testing
+import ArgParser
 
-final class ParamTests: XCTestCase {
+struct ParamTests {
     
-    func testFlagParam() throws {
+    @Test func testFlagParam() throws {
         let fa = FlagParam(names: [], help: "")
-        XCTAssertFalse(fa.value, "unset flag should be false")
+        #expect(!fa.value, "unset flag should be false")
         try fa.process(param: "")
-        XCTAssert(fa.value, "Set flag is true")
+        #expect(fa.value, "Set flag is true")
     }
     
-    func testIntParam() throws {
+    @Test func testIntParam() throws {
         let ia = BasicParam(names: [], initial: 2, help: "how many procs?")
-        XCTAssertEqual(ia.value, 2, "unset flag should be the initial value")
+        #expect(ia.value == 2, "unset flag should be the initial value")
         try ia.process(param: "abc", arg: "10")
-        XCTAssertEqual(ia.value, 10, "set flag for '10' should be 10")
+        #expect(ia.value == 10, "set flag for '10' should be 10")
         try ia.process(param: "abc", arg: "-10")
-        XCTAssertEqual(ia.value, -10, "set flag for '-10' should be -10")
-        XCTAssertThrowsError(try ia.process(param: "v", arg: "mm"), "non-integer arg should throw")
+        #expect(ia.value == -10, "set flag for '-10' should be -10")
+        #expect(throws: (any Error).self, "non-integer arg should throw") {
+            try ia.process(param: "v", arg: "mm")
+        }
     }
     
-    func testUIntParam() throws {
+    @Test func testUIntParam() throws {
         let uia = BasicParam<UInt8>(names: [], initial: 2, help: "how many procs?")
-        XCTAssertEqual(uia.value, 2, "unset flag should be the initial value")
+        #expect(uia.value == 2, "unset flag should be the initial value")
         try uia.process(param: "abc", arg: "10")
-        XCTAssertEqual(uia.value, 10, "set flag for '10' should be 10")
-        XCTAssertThrowsError(try uia.process(param: "v", arg: "300"), "out of range arg should throw")
-        XCTAssertThrowsError(try uia.process(param: "v", arg: "-3"), "out of range arg should throw")
-        XCTAssertThrowsError(try uia.process(param: "v", arg: "mm"), "non-integer arg should throw")
+        #expect(uia.value == 10, "set flag for '10' should be 10")
+        #expect(throws: (any Error).self, "out of range arg should throw") {
+            try uia.process(param: "v", arg: "300")
+        }
+        #expect(throws: (any Error).self, "out of range arg should throw") {
+            try uia.process(param: "v", arg: "-3")
+        }
+        #expect(throws: (any Error).self, "non-integer arg should throw") {
+            try uia.process(param: "v", arg: "mm")
+        }
     }
     
-    func testStringParam() throws {
+    @Test func testStringParam() throws {
         let sa = BasicParam(names: [], initial: "unset", help: "a string arg")
-        XCTAssertEqual(sa.value, "unset", "unset value should equal the initial value")
+        #expect(sa.value == "unset", "unset value should equal the initial value")
         try sa.process(param: "abc", arg: "string value")
-        XCTAssertEqual(sa.value, "string value", "set flag should equal what we set")
+        #expect(sa.value == "string value", "set flag should equal what we set")
     }
     
-    func testRangedIntParams() throws {
+    @Test func testRangedIntParams() throws {
         let ri = RangeLimitedParam(names: ["procs","p"], initial: 1, min: 0, max: 8, help: "how many processes to use")
-        XCTAssertEqual(ri.value, 1, "unset flag should be the initial value")
+        #expect(ri.value == 1, "unset flag should be the initial value")
         try ri.process(param: "abc", arg: "8")
-        XCTAssertEqual(ri.value, 8, "set flag for '8' should be 8")
-        XCTAssertThrowsError(try ri.process(param: "v", arg: "9"), "out of range arg should throw")
-        XCTAssertThrowsError(try ri.process(param: "v", arg: "xxx"), "non-integer value should throw")
+        #expect(ri.value == 8, "set flag for '8' should be 8")
+        #expect(throws: (any Error).self, "out of range arg should throw") {
+            try ri.process(param: "v", arg: "9")
+        }
+        #expect(throws: (any Error).self, "non-integer value should throw") {
+            try ri.process(param: "v", arg: "xxx")
+        }
     }
     
-    func testRangedStringParams() throws {
+    @Test func testRangedStringParams() throws {
         let rs = RangeLimitedParam(names: ["name","n"], initial: "care", min: "b", max: "d", help: "how many processes to use")
-        XCTAssertEqual(rs.value, "care", "unset flag should be the initial value")
+        #expect(rs.value == "care", "unset flag should be the initial value")
         try rs.process(param: "n", arg: "ballast")
-        XCTAssertEqual(rs.value, "ballast", "set flag for 'ballast' should be 'ballast'")
-        XCTAssertThrowsError(try rs.process(param: "v", arg: "azz"), "out of range arg should throw")
-        XCTAssertThrowsError(try rs.process(param: "v", arg: "daa"), "out of range arg should throw")
+        #expect(rs.value == "ballast", "set flag for 'ballast' should be 'ballast'")
+        #expect(throws: (any Error).self, "out of range arg should throw") {
+            try rs.process(param: "v", arg: "azz")
+        }
+        #expect(throws: (any Error).self, "out of range arg should throw") {
+            try rs.process(param: "v", arg: "daa")
+        }
     }
     
-    func testClamptedIntParams() throws {
+    @Test func testClamptedIntParams() throws {
         let ri = ClampedRangeParam(names: ["procs","p"], initial: 1, min: 0, max: 8, help: "how many processes to use")
-        XCTAssertEqual(ri.value, 1, "unset flag should be the initial value")
+        #expect(ri.value == 1, "unset flag should be the initial value")
         try ri.process(param: "abc", arg: "5")
-        XCTAssertEqual(ri.value, 5, "set flag for '5' should be 5")
+        #expect(ri.value == 5, "set flag for '5' should be 5")
         try ri.process(param: "abc", arg: "100")
-        XCTAssertEqual(ri.value, 8, "set flag for '100' should be clampted to 8")
+        #expect(ri.value == 8, "set flag for '100' should be clampted to 8")
         try ri.process(param: "abc", arg: "-2")
-        XCTAssertEqual(ri.value, 0, "set flag for '-2' should be clampted to 0")
+        #expect(ri.value == 0, "set flag for '-2' should be clampted to 0")
     }
 
-    func testAccumulatingParams() throws {
+    @Test func testAccumulatingParams() throws {
         let ap = AccumulatingParam(names: ["name"], initial: 0, help: "well hello there")
-        XCTAssertEqual(ap.value, 0, "unset param should be zero")
+        #expect(ap.value == 0, "unset param should be zero")
         try ap.process(param: "name")
-        XCTAssertEqual(ap.value, 1, "should increment")
+        #expect(ap.value == 1, "should increment")
         try ap.process(param: "name")
-        XCTAssertEqual(ap.value, 2, "should increment")
+        #expect(ap.value == 2, "should increment")
     }
 }
 
-final class ArgParserTests : XCTestCase {
-    func testParse1() throws {
+struct ArgParserTests {
+    @Test func testParse1() throws {
         let p = BasicParam(names: ["p"], initial: 0, help: "number of processes")
         let ap = ArgParser(p)
         let extras = try ap.parseArgs(["-p","20"])
-        XCTAssertEqual(p.value, 20, "given -p 20 should set it to 20")
-        XCTAssertTrue(extras.isEmpty, "no extra arguments were given in -p 20 case")
+        #expect(p.value == 20, "given -p 20 should set it to 20")
+        #expect(extras.isEmpty, "no extra arguments were given in -p 20 case")
     }
 
-    func testParse2() throws {
+    @Test func testParse2() throws {
         let p = BasicParam(names: ["parse"], initial: "cee", help: "language to parse")
         let ap = ArgParser(p)
         var extras = try ap.parseArgs(["--parse=awk"])
-        XCTAssertEqual(p.value, "awk", "given --parse=awk should set it to awk")
-        XCTAssertTrue(extras.isEmpty, "no extra arguments were given in --parse=awk case")
+        #expect(p.value == "awk", "given --parse=awk should set it to awk")
+        #expect(extras.isEmpty, "no extra arguments were given in --parse=awk case")
         extras = try ap.parseArgs(["--parse","bash"])
-        XCTAssertEqual(p.value, "bash", "given --parse bash should set it to bash")
-        XCTAssertTrue(extras.isEmpty, "no extra arguments were given in --parse=awk case")
+        #expect(p.value == "bash", "given --parse bash should set it to bash")
+        #expect(extras.isEmpty, "no extra arguments were given in --parse=awk case")
     }
 
-    func testParse3() throws {
+    @Test func testParse3() throws {
         let p = BasicParam(names: ["procs", "p"], initial: 0, help: "number of processes")
         let v = FlagParam(names: ["verbose", "v"], help: "verbose mode")
         let ap = ArgParser(p,v)
         let extras = try ap.parseArgs(["-vp","20"])
-        XCTAssertEqual(p.value, 20, "given -p 20 should set it to 20")
-        XCTAssertTrue(v.value, "given -v should set it to true")
-        XCTAssertTrue(extras.isEmpty, "no extra arguments were given in -vp 20 case")
+        #expect(p.value == 20, "given -p 20 should set it to 20")
+        #expect(v.value, "given -v should set it to true")
+        #expect(extras.isEmpty, "no extra arguments were given in -vp 20 case")
     }
 
-    func testParse4() throws {
+    @Test func testParse4() throws {
         let p = BasicParam(names: ["procs", "p"], initial: 0, help: "number of processes")
         let v = FlagParam(names: ["verbose", "v"], help: "verbose mode")
         let ap = ArgParser(p,v)
         let extras = try ap.parseArgs(["-vp", "--", "620", "--procs"])
-        XCTAssertEqual(p.value, 620, "given -p 620 should set it to 620")
-        XCTAssertTrue(v.value, "given -v should set it to true")
-        XCTAssertEqual(extras, ["--procs"], "extra arguments were given in -vp -- 620 --procs case")
+        #expect(p.value == 620, "given -p 620 should set it to 620")
+        #expect(v.value, "given -v should set it to true")
+        #expect(extras == ["--procs"], "extra arguments were given in -vp -- 620 --procs case")
     }
 
-    func testParse5() throws {
+    @Test func testParse5() throws {
         let p = BasicParam(names: ["procs", "p"], initial: 0, help: "number of processes")
         let v = FlagParam(names: ["verbose", "v"], help: "verbose mode")
         let ap = ArgParser(p,v)
         let args = ["cmdname", "--", "-vp", "--", "620", "--procs"]
         let extras = try ap.parseArgs(args.dropFirst())
-        XCTAssertEqual(p.value, 0, "-p should still have default")
-        XCTAssertFalse(v.value, "-v should till have default")
-        XCTAssertEqual(extras, Array(args.dropFirst(2)), "all args were verbatim")
+        #expect(p.value == 0, "-p should still have default")
+        #expect(!v.value, "-v should till have default")
+        #expect(extras == Array(args.dropFirst(2)), "all args were verbatim")
     }
 
-    func testParse6() throws {
+    @Test func testParse6() throws {
         let p = BasicParam(names: ["procs", "p"], initial: 0, help: "number of processes")
         let v = FlagParam(names: ["verbose", "v"], help: "verbose mode")
         let ap = ArgParser(p,v)
         let extras = try ap.parseArgs(["-v", "-"])
-        XCTAssertEqual(p.value, 0, "-p should still be default")
-        XCTAssertTrue(v.value, "given -v should set it to true")
-        XCTAssertEqual(extras, ["-"], "extra arguments '-' should be there")
+        #expect(p.value == 0, "-p should still be default")
+        #expect(v.value, "given -v should set it to true")
+        #expect(extras == ["-"], "extra arguments '-' should be there")
     }
 
 }
--- Tests/ArgParserTests/StringConvertibleTests.swift
+++ Tests/ArgParserTests/StringConvertibleTests.swift
@@ -1,182 +1,179 @@
 import Foundation
-import XCTest
-@testable import ArgParser
+import Testing
+import ArgParser
 
 /**
  Tessts of BasicParam with IntegerSequenceArg
  */
-final class IntSeqArgTests : XCTestCase {
+struct IntSeqArgTests {
     var isa : BasicParam<IntegerSequenceArg>?
     
-    override func setUp() {
-        super.setUp()
+    init() {
         isa = BasicParam<IntegerSequenceArg>(names: ["isa"], initial: IntegerSequenceArg(), help: "")
     }
     
-    func testDefaultEmpty() throws {
-        XCTAssertEqual(Array(isa!.value.seq), [])
+    @Test func testDefaultEmpty() throws {
+        #expect(Array(isa!.value.seq) == [])
     }
     
-    func testSingleVal() throws {
+    @Test func testSingleVal() throws {
         try isa!.process(param: "isa", arg: "1")
-        XCTAssertEqual(Array(isa!.value.seq),[1])
+        #expect(Array(isa!.value.seq) == [1])
     }
 
-    func testSingleVal2() throws {
+    @Test func testSingleVal2() throws {
         try isa!.process(param: "isa", arg: "-14")
-        XCTAssertEqual(Array(isa!.value.seq),[-14])
+        #expect(Array(isa!.value.seq) == [-14])
     }
 
-    func testSingleRange() throws {
+    @Test func testSingleRange() throws {
         try isa!.process(param: "isa", arg: "-2..2")
-        XCTAssertEqual(Array(isa!.value.seq),[-2,-1,0,1,2])
+        #expect(Array(isa!.value.seq) == [-2,-1,0,1,2])
     }
 
-    func testSingleRange2() throws {
+    @Test func testSingleRange2() throws {
         try isa!.process(param: "isa", arg: "10..12")
-        XCTAssertEqual(Array(isa!.value.seq),[10,11,12])
+        #expect(Array(isa!.value.seq) == [10,11,12])
     }
 
     // right now, we don't support backwards ranges... maybe in the future find
     // a way to allow it
-    func testBackwardRange() throws {
-        XCTAssertThrowsError(try isa!.process(param: "isa", arg: "14..12"))
+    @Test func testBackwardRange() throws {
+        #expect(throws: (any Error).self) { try isa!.process(param: "isa", arg: "14..12") }
     }
     
-    func testNonNumber() throws {
-        XCTAssertThrowsError(try isa!.process(param: "isa", arg: "14a"))
+    @Test func testNonNumber() throws {
+        #expect(throws: (any Error).self) { try isa!.process(param: "isa", arg: "14a") }
     }
 
-    func testMultipleNums() throws {
+    @Test func testMultipleNums() throws {
         try isa!.process(param: "isa", arg: "1,2,3")
-        XCTAssertEqual(Array(isa!.value.seq), [1,2,3])
+        #expect(Array(isa!.value.seq) == [1,2,3])
     }
     
-    func testMultipleNums2() throws {
+    @Test func testMultipleNums2() throws {
         try isa!.process(param: "isa", arg: "1,-5,3")
-        XCTAssertEqual(Array(isa!.value.seq), [1,-5,3])
+        #expect(Array(isa!.value.seq) == [1,-5,3])
     }
 
-    func testMultipleNums3() throws {
+    @Test func testMultipleNums3() throws {
         try isa!.process(param: "isa", arg: "1,10..12,3")
-        XCTAssertEqual(Array(isa!.value.seq), [1,10,11,12,3])
+        #expect(Array(isa!.value.seq) == [1,10,11,12,3])
     }
 
-    func testEmptyArg() throws {
+    @Test func testEmptyArg() throws {
         try isa!.process(param: "isa", arg: "")
-        XCTAssertEqual(Array(isa!.value.seq), [])
+        #expect(Array(isa!.value.seq) == [])
     }
     
-    func testStringConversion() throws {
+    @Test func testStringConversion() throws {
         try isa!.process(param: "isa", arg: "-10,14..16,18..20")
-        XCTAssertEqual(isa!.value.description, "-10,14,15,16,18,19,20")
+        #expect(isa!.value.description == "-10,14,15,16,18,19,20")
     }
     
-    func testStringConversionWhenEmpty() throws {
+    @Test func testStringConversionWhenEmpty() throws {
         try isa!.process(param: "isa", arg: "")
-        XCTAssertEqual(isa!.value.description, "")
+        #expect(isa!.value.description == "")
     }
     
-    func testArgConstructor() throws {
+    @Test func testArgConstructor() throws {
         let arg = IntegerSequenceArg([1...3,6...6])
-        XCTAssertEqual(arg.description, "1,2,3,6")
+        #expect(arg.description == "1,2,3,6")
     }
 
 }
 
-
 /**
  Tessts of DateArg
  */
-final class YMDArgTests : XCTestCase {
+struct YMDArgTests  {
     var da : BasicParam<YMDArg>?
     
-    override func setUp() {
-        super.setUp()
+    init() {
         da = BasicParam<YMDArg>(names: ["da"], initial: YMDArg(), help: "")
     }
     
-    func testDefaultSetting() throws {
+    @Test func testDefaultSetting() throws {
         let today = Calendar.current.startOfDay(for: Date())
-        XCTAssertEqual(da!.value.date, today)
+        #expect(da!.value.date == today)
     }
     
-    func testTPlusTMinusDates() throws {
+    @Test func testTPlusTMinusDates() throws {
         var dc = Calendar.current.dateComponents([.year,.month,.day], from: Date())
         try da!.process(param: "da", arg: "t")
-        XCTAssertEqual(da!.value, YMDArg(year: dc.year!, month: dc.month!, day: dc.day!))
+        #expect(da!.value == YMDArg(year: dc.year!, month: dc.month!, day: dc.day!))
 
         try da!.process(param: "da", arg: "t+0")
-        XCTAssertEqual(da!.value, YMDArg(year: dc.year!, month: dc.month!, day: dc.day!))
+        #expect(da!.value == YMDArg(year: dc.year!, month: dc.month!, day: dc.day!))
 
         try da!.process(param: "da", arg: "t-0")
-        XCTAssertEqual(da!.value, YMDArg(year: dc.year!, month: dc.month!, day: dc.day!))
+        #expect(da!.value == YMDArg(year: dc.year!, month: dc.month!, day: dc.day!))
         
         dc = Calendar.current.dateComponents([.year,.month,.day], from: Calendar.current.date(byAdding: .day, value: 5, to: Date())!)
         try da!.process(param: "da", arg: "t+5")
-        XCTAssertEqual(da!.value, YMDArg(year: dc.year!, month: dc.month!, day: dc.day!))
+        #expect(da!.value == YMDArg(year: dc.year!, month: dc.month!, day: dc.day!))
 
         dc = Calendar.current.dateComponents([.year,.month,.day], from: Calendar.current.date(byAdding: .day, value: -5, to: Date())!)
         try da!.process(param: "da", arg: "t-5")
-        XCTAssertEqual(da!.value, YMDArg(year: dc.year!, month: dc.month!, day: dc.day!))
+        #expect(da!.value == YMDArg(year: dc.year!, month: dc.month!, day: dc.day!))
     }
     
-    func testSimpleDates() throws {
+    @Test func testSimpleDates() throws {
         try da!.process(param: "da", arg: "2011-01-01")
-        XCTAssertEqual(da!.value, YMDArg(year: 2011, month: 1, day: 1)!)
+        #expect(da!.value == YMDArg(year: 2011, month: 1, day: 1)!)
         try da!.process(param: "da", arg: "1977-06-02")
-        XCTAssertEqual(da!.value, YMDArg(year: 1977, month: 6, day: 2)!)
+        #expect(da!.value == YMDArg(year: 1977, month: 6, day: 2)!)
     }
     
-    func testAbbreviatedDates() throws {
+    @Test func testAbbreviatedDates() throws {
         let dc = Calendar.current.dateComponents([.year,.month], from: Date())
         try da!.process(param: "da", arg: "11-14")
-        XCTAssertEqual(da!.value.description, "\(dc.year!)-11-14")
+        #expect(da!.value.description == "\(dc.year!)-11-14")
         
         try da!.process(param: "da", arg: "14")
-        XCTAssertEqual(da!.value.description, "\(dc.year!)-\(dc.month!)-14")
+        #expect(da!.value.description == "\(dc.year!)-\(dc.month!)-14")
     }
     
-    func testSpecialArgs() throws {
+    @Test func testSpecialArgs() throws {
         let today = Calendar.current.startOfDay(for: Date())
         try da!.process(param: "da", arg: "Today")
-        XCTAssertEqual(da!.value.date, today)
+        #expect(da!.value.date == today)
         
         let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: today)!
         try da!.process(param: "da", arg: "yesterDAY")
-        XCTAssertEqual(da!.value, YMDArg(yesterday))
+        #expect(da!.value == YMDArg(yesterday))
         
         let tomorrow =  Calendar.current.date(byAdding: .day, value: 1, to: today)!
         try da!.process(param: "da", arg: "TOmorroW")
-        XCTAssertEqual(da!.value, YMDArg(tomorrow))
+        #expect(da!.value == YMDArg(tomorrow))
     }
     
-    func testBadDates() throws {
-        XCTAssertNil(YMDArg("2011-21-blah"))
-        XCTAssertNil(YMDArg(""))
-        XCTAssertNil(YMDArg("1-2-2011"))
-        XCTAssertNil(YMDArg("12-32"))
-        XCTAssertNil(YMDArg("0-0-0"))
-        XCTAssertNil(YMDArg("2018-0-14"))
+    @Test func testBadDates() throws {
+        #expect(YMDArg("2011-21-blah") == nil)
+        #expect(YMDArg("") == nil)
+        #expect(YMDArg("1-2-2011") == nil)
+        #expect(YMDArg("12-32") == nil)
+        #expect(YMDArg("0-0-0") == nil)
+        #expect(YMDArg("2018-0-14") == nil)
     }
     
-    func testClampableDates() throws {
+    @Test func testClampableDates() throws {
         let cda = ClampedRangeParam(
             names: ["date"],
             initial: YMDArg(year:2022,month:12,day:1)!,
             min: YMDArg(year:2022,month:11,day:15)!,
             max: YMDArg(year:2022,month:12,day:15)!,
             help: "between nov 15 and dec 15 in year 2022")
-        XCTAssertEqual(cda.value, YMDArg(year:2022,month:12,day:1))
+        #expect(cda.value == YMDArg(year:2022,month:12,day:1))
         try cda.process(param: "date", arg: "2022-11-14")
-        XCTAssertEqual(cda.value, YMDArg(year:2022,month:11,day:15))
+        #expect(cda.value == YMDArg(year:2022,month:11,day:15))
         try cda.process(param: "date", arg: "2012-1-1")
-        XCTAssertEqual(cda.value, YMDArg(year:2022,month:11,day:15))
+        #expect(cda.value == YMDArg(year:2022,month:11,day:15))
         try cda.process(param: "date", arg: "2000-12-3")
-        XCTAssertEqual(cda.value, YMDArg(year:2022,month:11,day:15))
+        #expect(cda.value == YMDArg(year:2022,month:11,day:15))
         try cda.process(param: "date", arg: "2023-12-1")
-        XCTAssertEqual(cda.value, YMDArg(year:2022,month:12,day:15))
+        #expect(cda.value == YMDArg(year:2022,month:12,day:15))
         try cda.process(param: "date", arg: "2022-12-16")
-        XCTAssertEqual(cda.value, YMDArg(year:2022,month:12,day:15))
+        #expect(cda.value == YMDArg(year:2022,month:12,day:15))
     }
 }
